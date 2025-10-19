8262b6bce683b7ab64d7a4f01f848d01bab3501c
AzmiZakaria <zakariaazmi45@gmail.com>
2025-10-19 18:53:41 +0100

    l'entité  BankAccount

---
+++ src/main/java/com/azmi/bankaccountservice/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package com.azmi.bankaccountservice.entities;
+
+import com.azmi.bankaccountservice.enums.AccountType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private String currency;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+
+}
---
+++ src/main/java/com/azmi/bankaccountservice/enums/AccountType.java
@@ -0,0 +1,5 @@
+package com.azmi.bankaccountservice.enums;
+
+public enum AccountType {
+    SAVING_ACCOUNT, CURRENT_ACCOUNT
+}
